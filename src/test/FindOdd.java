f4762f27ffedccf288dc5b9c9c3df5ecb8a66a0e
SergiiBal <sergey.balashchuk@gmail.com>
2022-05-16 22:04:04 +0300

    Commented out FindOdd.java

--- src/test/FindOdd.java
+++ src/test/FindOdd.java
@@ -1,23 +1,23 @@
-package test;
-
-public class FindOdd {
-    public static int findIt(int[] a) {
-        int odd = a[1];
-        int n = 0;
-        int x;
-        for (x = 1; x <= a.length; x = x + 1) {
-            if (odd == a[x]) {
-                n++;}}
-        if (n % 2 == 0 || x< a.length) {
-            odd = a[x+1];
-        }
-        return odd;
-    }
-}
-
-    public static void main(String[] args) {
-        int result = new int[] ({10});
-        System.out.println(result);
-    }
-}
-
+//package test;
+//
+//public class FindOdd {
+//    public static int findIt(int[] a) {
+//        int odd = a[1];
+//        int n = 0;
+//        int x;
+//        for (x = 1; x <= a.length; x = x + 1) {
+//            if (odd == a[x]) {
+//                n++;}}
+//        if (n % 2 == 0 || x< a.length) {
+//            odd = a[x+1];
+//        }
+//        return odd;
+//    }
+//}
+//
+//    public static void main(String[] args) {
+//        int result = new int[] ({10});
+//        System.out.println(result);
+//    }
+//}
+//
