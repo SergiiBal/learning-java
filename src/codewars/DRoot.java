5073062122afe58040537ad95f9c3a8dc6143acb
SergiiBal <sergey.balashchuk@gmail.com>
2022-05-19 12:42:32 +0300

    My first try after break. Finished!!

--- src/codewars/DRoot.java
+++ src/codewars/DRoot.java
@@ -3,20 +3,25 @@ package codewars;
 public class DRoot {
     public static void digital_root(int n) {
         String temp = Integer.toString(n);
-        int[] arrayNumbers = new int[temp.length()];
+
         int sum = 0;
-        if
-        for (int i = 0; i < temp.length(); i++) {
+        while (temp.length() > 1) {
+            int[] arrayNumbers = new int[temp.length()];
+            for (int i = 0; i < temp.length(); i++) {
                 arrayNumbers[i] = temp.charAt(i) - '0';
-            System.out.println(arrayNumbers[i]);
-            sum = sum +  arrayNumbers[i];
+             //   System.out.println(arrayNumbers[i]);
+                sum = sum + arrayNumbers[i];
             }
-        System.out.println(sum);
-        n = sum;
+          //  System.out.println(sum);
+            n = sum;
+            temp = Integer.toString(n);
+            sum = 0;
         }
+        System.out.println(n);
+    }
 
     public static void main(String[] args) {
-        digital_root(16);
+        digital_root(132189);
     }
 }
 
