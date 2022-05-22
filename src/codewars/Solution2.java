eac1b617fbf24950c78c2ac61cf3d5ab1f4f48fa
SergiiBal <sergey.balashchuk@gmail.com>
2022-05-22 20:50:38 +0300

    Could you help please. I cant use "numbers"

---
+++ src/codewars/Solution2.java
@@ -0,0 +1,31 @@
+package codewars;
+
+public class Solution2 {
+    public static int[] twoSum(int[] numbers, int target);
+    int sum = 0;
+    int a;
+    int b;
+    int[] resultArray = null;
+    System.out.println(numbers.length);
+ for (int i=0; i<=numbers.length; i++)     {
+        for (int j = 0; j <= numbers.length; i++) {
+            sum = number[i] + number[j];
+            System.out.println(sum);
+            if (sum == target) {
+                    resultArray[0] = a;
+                    resultArray[1] = b;
+                }
+            }
+        }
+ return resultArray;
+
+        //     return null; // Do your magic!
+
+
+
+    public static void main(String[] args) {
+       int[] twoSum(new int[]{1, 2, 3},4);
+    }
+}
+
+
