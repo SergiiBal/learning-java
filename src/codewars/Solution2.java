17bc37e8155e3539c5da1287c400aeaa485206a3
SergiiBal <sergey.balashchuk@gmail.com>
2022-05-26 21:17:20 +0300

    Thank you very much! Well done!

--- src/codewars/Solution2.java
+++ src/codewars/Solution2.java
@@ -1,30 +1,33 @@
 package codewars;
 
 public class Solution2 {
-    public static int[] twoSum(int[] numbers, int target);
-    int sum = 0;
-    int a;
-    int b;
-    int[] resultArray = null;
-    System.out.println(numbers.length);
- for (int i=0; i<=numbers.length; i++)     {
-        for (int j = 0; j <= numbers.length; i++) {
-            sum = number[i] + number[j];
-            System.out.println(sum);
-            if (sum == target) {
-                    resultArray[0] = a;
-                    resultArray[1] = b;
+    public static int[] twoSum(int[] numbers, int target) {
+        int sum = 0;
+        int[] resultArray = new int[2];
+    //    System.out.println(numbers.length);
+if (numbers == null) {
+            System.out.println("Received a null");
+        } if (numbers.length < 2) {
+            System.out.println("Received an array that's not of length 2");
+        } else
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                sum = numbers[i] + numbers[j];
+               // System.out.println(sum);
+                if (sum == target) {
+                    resultArray[0] = i;
+                    resultArray[1] = j;
                 }
             }
         }
- return resultArray;
-
-        //     return null; // Do your magic!
-
-
+        return resultArray;
+    }
 
     public static void main(String[] args) {
-       int[] twoSum(new int[]{1, 2, 3},4);
+        int[] p = twoSum(new int[]{1, 2, 3}, 4);
+        for (int k = 0; k <= 1; k++) {
+            System.out.println(p[k]);
+        }
     }
 }
 
