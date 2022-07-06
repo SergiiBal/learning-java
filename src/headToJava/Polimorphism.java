b0b466ca710c20628d5fb83abf24ada1ecd63757
SergiiBal <sergey.balashchuk@gmail.com>
2022-07-06 10:07:07 +0300

    Working Hard

--- src/headToJava/Polimorphism.java
+++ src/headToJava/Polimorphism.java
@@ -11,17 +11,26 @@ class A {
     void m3() {
         System.out.println("A's m3, ");
     }
+    static void s1(){
+        System.out.println("A's s1,  ");
+    }
 }
 
 class B extends A {
     void m1() {
         System.out.println("B's m1, ");
     }
+    static void s1(){
+        System.out.println("B's s1,  ");
+    }
 }
 
 class C extends  B {
     void m3() {
         System.out.println("C's m3, " + (ivar + 6));
+    }
+    static void s1(){
+        System.out.println("C's s1,  ");
     }
 }
 
@@ -31,17 +40,34 @@ public class Polimorphism {
         B b = new B();
         C c = new C();
         A a2 = new C();
-        b.m1();
+//        b.m1();
 //        c.m2();
 //        a.m3();
-        c.m1();
+//        c.m1();
 //        c.m2();
 //        c.m3();
-        a.m1();
+//        a.m1();
 //        b.m2();
 //        c.m3();
-        a2.m1();
+//        a2.m1();
 //        a2.m2();
 //        a2.m3();
+        // Наступні 4 рядки це приклад поліморфізму, коли для методу callm1(A a) передаються перемінні a, b, c, a2
+        callm1(a);
+        callm1(b);
+        callm1(c);
+        callm1(a2);
+        // Статичні перевіряють те що зліва від перемінної (тип А, B, C)
+        // Обєктні перевіряють по назві обєкта
+//        a2.s1();
+//        a.s1();
+//        c.s1();
+//        B.s1();
+//        A.s1();
+    }
+    static  void callm1(A a) {
+        a.m1();
     }
 }
+
+
